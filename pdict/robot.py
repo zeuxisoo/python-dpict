96c98fdca63b05f4595ca47336e882d719461a79
Zeuxis Lo <seekstudio@gmail.com>
2015-04-30 18:33:07 +0800

    convert the robot.py into class object

--- pdict/robot.py
+++ pdict/robot.py
@@ -7,55 +7,54 @@ from urllib2 import urlopen
 from mimetypes import guess_type
 from pyquery import PyQuery as pq
 
-def main(word):
-    d = pq(url="http://www.onlinedict.com/servlet/MobiDictLookup14?WoRd={0}&example=true&phrase=true&from=prev".format(quote_plus(word)))
+class Robot(object):
 
-    table     = d("table tr:nth-child(2) td")
-    moccasin  = table.find("tr[bgcolor=moccasin] td font b").text()
-    beige     = table.find("tr[bgcolor=beige] td font b").text()
-    next_word = table.find("tr:nth-last-child(5) td a").html();
+    def __init__(self):
+        pass
 
-    print moccasin
-    print beige
-    print next_word
+    def create_database(self):
+        from .models import Word
+        Word.create_table()
 
-    # Remove 4 links
-    table.remove("tr[bgcolor=moccasin] td[align=center] a")
+    def run(self, word):
+        d = pq(url="http://www.onlinedict.com/servlet/MobiDictLookup14?WoRd={0}&example=true&phrase=true&from=prev".format(quote_plus(word)))
 
-    # Remove "looking for" keyboard
-    table.remove("tr:first-child")
+        table     = d("table tr:nth-child(2) td")
+        moccasin  = table.find("tr[bgcolor=moccasin] td font b").text()
+        beige     = table.find("tr[bgcolor=beige] td font b").text()
+        next_word = table.find("tr:nth-last-child(5) td a").html();
 
-    # Remove last 3 lines
-    for i in xrange(0, 5):
-        table.remove("tr:last-child")
+        print moccasin
+        print beige
+        print next_word
 
-    # If found PREVIOUS WORD tr, remove again
-    if table.find("tr:last-child td").text().find("PREVIOUS WORD") == 0:
-        table.remove("tr:last-child")
+        # Remove 4 links
+        table.remove("tr[bgcolor=moccasin] td[align=center] a")
 
-    # Convert phonetic image to data uri
-    for phonetic_img in table.find("tr:nth-child(2) td img"):
-        image_tag = pq(phonetic_img)
-        image_url = "{0}{1}".format("http://www.onlinedict.com/", image_tag.attr('src').replace("../", ""))
+        # Remove "looking for" keyboard
+        table.remove("tr:first-child")
 
-        image_uri = "data:{0};base64,{1}".format(
-            guess_type(image_url, strict=True)[0],
-            urlopen(image_url).read().encode("base64").replace("\n","")
-        )
+        # Remove last 3 lines
+        for i in xrange(0, 5):
+            table.remove("tr:last-child")
 
-        image_tag.attr("src", image_uri)
+        # If found PREVIOUS WORD tr, remove again
+        if table.find("tr:last-child td").text().find("PREVIOUS WORD") == 0:
+            table.remove("tr:last-child")
 
-    # Save html
-    # with open("./{0}.html".format(word), 'w+') as f:
-    #     f.write(table.html().encode("utf-8"))
-    #     f.close()
+        # Convert phonetic image to data uri
+        for phonetic_img in table.find("tr:nth-child(2) td img"):
+            image_tag = pq(phonetic_img)
+            image_url = "{0}{1}".format("http://www.onlinedict.com/", image_tag.attr('src').replace("../", ""))
 
-def create_database():
-    from models import Word
+            image_uri = "data:{0};base64,{1}".format(
+                guess_type(image_url, strict=True)[0],
+                urlopen(image_url).read().encode("base64").replace("\n","")
+            )
 
-    Word.create_table()
+            image_tag.attr("src", image_uri)
 
-if __name__ == "__main__":
-    # create_database()
-
-    main("door")
+        # Save html
+        # with open("./{0}.html".format(word), 'w+') as f:
+        #     f.write(table.html().encode("utf-8"))
+        #     f.close()
