9ef31d8657affcba0e1dc94a1dcc0a1f65f90a64
Zeuxis Lo <seekstudio@gmail.com>
2015-05-04 15:29:42 +0800

    make all column can null

--- pdict/models/word.py
+++ pdict/models/word.py
@@ -3,10 +3,10 @@ from peewee import CharField, TextField, DateTimeField
 from .base import BaseModel
 
 class Word(BaseModel):
-    moccasin      = CharField(max_length=50)
-    beige         = CharField(max_length=100)
-    previous_word = CharField(max_length=50)
-    next_word     = CharField(max_length=50)
-    content       = TextField()
+    moccasin      = CharField(max_length=50, null=True)
+    beige         = CharField(max_length=100, null=True)
+    previous_word = CharField(max_length=50, null=True)
+    next_word     = CharField(max_length=50, null=True)
+    content       = TextField(null=True)
     create_at     = DateTimeField(default=datetime.now)
     update_at     = DateTimeField(default=datetime.now)
