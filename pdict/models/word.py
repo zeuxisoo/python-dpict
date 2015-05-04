71d2e2648c3bf4d4f214cdead74d724b16ad1ead
Zeuxis Lo <seekstudio@gmail.com>
2015-05-04 14:59:05 +0800

    add previous word column

--- pdict/models/word.py
+++ pdict/models/word.py
@@ -3,9 +3,10 @@ from peewee import CharField, TextField, DateTimeField
 from .base import BaseModel
 
 class Word(BaseModel):
-    moccasin  = CharField(max_length=50)
-    beige     = CharField(max_length=100)
-    next_word = CharField(max_length=50)
-    content   = TextField()
-    create_at = DateTimeField(default=datetime.now)
-    update_at = DateTimeField(default=datetime.now)
+    moccasin      = CharField(max_length=50)
+    beige         = CharField(max_length=100)
+    previous_word = CharField(max_length=50)
+    next_word     = CharField(max_length=50)
+    content       = TextField()
+    create_at     = DateTimeField(default=datetime.now)
+    update_at     = DateTimeField(default=datetime.now)
